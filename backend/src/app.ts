51fc26bea9637b8ee5e5eea7d3915dfdd584b25d
mirjalol <mirislomovmirjalol@gmail.com>
2023-07-20 11:34:08 +0100

    http errors library set up

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,8 @@
 import "dotenv/config";
 import express, {NextFunction, Request, Response} from "express";
-import {Error} from "mongoose";
 import notesRouter from "./routes/api/notes";
 import morgan from "morgan";
+import createHttpError, {isHttpError} from "http-errors";
 
 const app = express();
 app.use(morgan("dev"));
@@ -11,17 +11,19 @@ app.use(express.json());
 app.use("/api/notes", notesRouter);
 
 app.use((req, res, next) => {
-    next(new Error("Not found"));
+    next(createHttpError(404, "Endpoint not found"));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     console.error(error);
     let errorMessage = "An error occurred";
-    if (error instanceof Error) {
+    let statusCode = 500;
+    if (isHttpError(error)) {
+        statusCode = error.statusCode;
         errorMessage = error.message;
     }
-    res.status(500).json({error: errorMessage});
+    res.status(statusCode).json({error: errorMessage});
 
 })
 
