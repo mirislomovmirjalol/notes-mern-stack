b4822747d0935d025e5dfd026441a9c879297729
mirjalol <mirislomovmirjalol@gmail.com>
2023-07-25 10:12:50 +0100

    Auth set up for backend

--- backend/src/util/validateEnv.ts
+++ backend/src/util/validateEnv.ts
@@ -3,5 +3,6 @@ import {cleanEnv, port, str} from "envalid";
 export default cleanEnv(process.env, {
     MONGO_CONNECTION_STRING: str(),
     PORT: port(),
-    FRONTEND_URL: str()
+    FRONTEND_URL: str(),
+    SESSION_SECRET: str(),
 });
