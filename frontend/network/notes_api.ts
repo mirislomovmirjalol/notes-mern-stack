753c36558eea7947c0ff66622b7284d50c93fda7
mirjalol <mirislomovmirjalol@gmail.com>
2023-08-05 10:11:58 +0100

    auth, front end env

--- frontend/network/notes_api.ts
+++ frontend/network/notes_api.ts
@@ -1,4 +1,5 @@
 import {Note} from "@/models/Note";
+import {User} from "@/models/User";
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
     const response = await fetch(input, init)
@@ -11,8 +12,57 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
     return data
 }
 
+export async function getAuthUser(): Promise<User> {
+    const response = await fetchData(process.env.BACKEND_URL + "/api/users/", {
+        method: "GET",
+    })
+    return response
+}
+
+export interface SignupInput {
+    username: string
+    email: string
+    password: string
+}
+
+export async function signup(signupInput: SignupInput): Promise<User> {
+    const response = await fetchData(process.env.BACKEND_URL + "/api/users/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(signupInput),
+    })
+    return response
+}
+
+export interface LoginInput {
+    username: string
+    password: string
+}
+
+export async function login(loginInput: LoginInput): Promise<User> {
+    const response = await fetchData(process.env.BACKEND_URL + "/api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginInput),
+    })
+    return response
+}
+
+export async function logout(): Promise<void> {
+    await fetchData(process.env.BACKEND_URL + "/api/users/logout", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    })
+}
+
 export async function fetchNotes(): Promise<Note[]> {
-    const response = await fetchData("http://localhost:3001/api/notes/", {
+    const response = await fetchData(process.env.BACKEND_URL + "/api/notes/", {
         method: "GET",
     })
     return response
@@ -24,7 +74,7 @@ export interface NoteInput {
 }
 
 export async function createNote(note: NoteInput): Promise<Note> {
-    const response = await fetchData("http://localhost:3001/api/notes/", {
+    const response = await fetchData(process.env.BACKEND_URL + "/api/notes/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -35,7 +85,7 @@ export async function createNote(note: NoteInput): Promise<Note> {
 }
 
 export async function deleteNote(noteId: string): Promise<void> {
-    await fetchData(`http://localhost:3001/api/notes/${noteId}`, {
+    await fetchData(process.env.BACKEND_URL + `/api/notes/${noteId}`, {
         method: "DELETE",
         headers: {
             "Content-Type": "application/json",
@@ -44,7 +94,7 @@ export async function deleteNote(noteId: string): Promise<void> {
 }
 
 export async function updateNote(noteId: string, note: NoteInput): Promise<Note> {
-    const response = await fetchData(`http://localhost:3001/api/notes/${noteId}`, {
+    const response = await fetchData(process.env.BACKEND_URL + `/api/notes/${noteId}`, {
         method: "PATCH",
         headers: {
             "Content-Type": "application/json",
